75c8f6f850549d400b752e9b892128856c056565
ry <ry@DESKTOP-32PH758.localdomain>
2024-02-21 23:25:25 -0800

    utilizing pythons socket library to convert hostname to ipv4 and added readme

---
+++ portscanner.py
@@ -0,0 +1,18 @@
+import socket
+
+def scan_port(ip, port):
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.settimeout(1)  
+            s.connect((ip, port))
+            print(f"{port} is open")
+    except:
+        pass  
+
+if __name__ == "__main__":
+    target = "www.google.com"
+    target_ip = socket.gethostbyname(target)
+    print(f"Starting scan on host: {target_ip}")
+    
+    for port in range(1, 1025): 
+        scan_port(target_ip, port)
